1b32dc78224ea5b0ed8bd12da0fa7bd5a338bcaf
Nubal Manhas <113201793+nmanhas2@users.noreply.github.com>
2024-01-20 16:49:42 -0700

    Delete Projects directory

--- Projects/ATmega32/adc.h
+++
@@ -1,3 +0,0 @@
-
-void adcinit();
-int adcread(char);
--- Projects/lib/LTC2633.h
+++
@@ -1,6 +0,0 @@
-// header for LTC2633 Dual DAC
-// Simon Walker, NAIT
-// Revision History:
-// March 22 2022 - Initial Build
-
-int LTC2633_Write (unsigned int iVal);
